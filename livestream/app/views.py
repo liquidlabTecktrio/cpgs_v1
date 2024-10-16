df5f862361e1f4a41e3dd9342300dc5a8042cdd1
tecktrio <amalpullan4@gmail.com>
2024-10-16 21:57:36 +0530

    first commit

---
+++ livestream/app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+import cv2
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+
+def video_stream():
+    # Open the camera
+    camera = cv2.VideoCapture(1)  # Change to the appropriate camera index if needed
+    while True:
+        # Read frame from the camera
+        success, frame = camera.read()
+        if not success:
+            break
+        # Encode the frame as JPEG
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        # Yield the frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def index(request):
+    return render(request, 'index.html')
+
+def stream(request):
+    return StreamingHttpResponse(video_stream(), content_type='multipart/x-mixed-replace; boundary=frame')
---
+++ test.py
@@ -0,0 +1,27 @@
+import re
+import easyocr
+import cv2
+import SlotImages
+
+slotImage = cv2.imread(f'SlotImages/indiapl8.webp')
+slotingray = cv2.cvtColor(slotImage, cv2.COLOR_BGR2GRAY)
+blured = cv2.GaussianBlur(slotingray, (5,5),0)
+cv2.imshow('image original',slotImage)
+cv2.imshow('image in gray',slotingray)
+cv2.imshow('image in gray',blured)
+
+
+reg = r'[A-Z]{2}\s?[0-9]{2}[A-Z]{1}\s?[0-9]{4}'
+
+
+
+
+if cv2.waitKey(10) & 0xFF == ord('q'):
+    pass
+reader = easyocr.Reader(model_storage_directory = '/Users/apple/Desktop/ParkoV1/ParkoV2/LanguageModels', lang_list = ['en'])
+result = reader.readtext(blured)
+# print(result)
+for i in result:
+    # print(i[1])
+    if re.match(reg, i[1]):
+        print(i[1])
