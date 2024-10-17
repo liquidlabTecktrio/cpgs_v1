14313a0dfe9f914b01f335b8ae7b9b1cfc941a9e
tecktrio <amalpullan4@gmail.com>
2024-10-17 11:54:58 +0530

    test

--- test.py
+++ test.py
@@ -11,19 +11,19 @@ def find_working_camera():
         camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Set to a supported width
         camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # Set to a supported height
         if camera.isOpened():
-            print(f'Found working camera at {device}')
+            print(f'Found working camera at ')
             # Optional: Capture a frame to confirm it's working
             ret, frame = camera.read()
             if ret:
-                print(f'Successfully captured a frame from {device}')
+                print(f'Successfully captured a frame from ')
                 # Display the frame or process it as needed
                 # cv2.imshow(f'Camera Feed from {device}', frame)
                 # cv2.waitKey(0)  # Wait for a key press to close the window
             else:
-                print(f'Camera at {device} could not capture a frame.')
+                print(f'Camera atcould not capture a frame.')
             camera.release()
         else:
-            print(f'Camera at {device} not found.')
+            print(f'Camera at  not found.')
 
 find_working_camera()
 # cv2.destroyAllWindows()
