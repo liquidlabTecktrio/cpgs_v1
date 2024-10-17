dad7dce6f92803428fc76f495544e26498519dce
tecktrio <amalpullan4@gmail.com>
2024-10-17 08:45:36 +0530

    camera port test

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 import cv2
 camera = cv2.VideoCapture()
+
+
+def load_next_frame():
+    '''
+    Frame will be assigned with a latest camera frame or image
+    '''
+    if not camera.isOpened():
+        print('Camera not found')
+        return False
+    else:
+        __, frame = camera.read()
+        return True
+    
+
+while True:
+    load_next_frame()
