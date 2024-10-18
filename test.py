044d05e32721151ca70bf3407a679456064dd10a
tecktrio <amalpullan4@gmail.com>
2024-10-18 10:13:02 +0530

    test

--- test.py
+++ test.py
@@ -4,34 +4,9 @@ from picamera2 import Picamera2
 cap = Picamera2()
 cap.start()
 
-# import cv2
-import time
 
-# Set up video capture (0 for the default camera)
-# cap = cv2.VideoCapture(0)
-
-# Define video codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
-
-# Capture video for 5 seconds
-start_time = time.time()
-while int(time.time() - start_time) < 5:
-    frame = cap.capture_array()
+frame = cap.capture_array()
+with open('image.jpg','rw') as file:
+    file.write(frame)
 
    
-    # Write the frame to the video file
-    out.write(frame)
-
-    # Display the frame (optional)
-    # cv2.imshow('frame', frame)
-
-    # Press 'q' to exit early
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-
-# Release the camera and close the window
-# cap.release()
-out.release()
-# cv2.destroyAllWindows()
