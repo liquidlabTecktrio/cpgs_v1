770014510179527d9ce54a2d4702c41de8b6501a
tecktrio <amalpullan4@gmail.com>
2024-10-20 16:50:32 +0530

    ee

--- cpgswebinterface/src/App.js
+++ cpgswebinterface/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import { HashRouter as Router, Route,Routes } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Loginpage from "./Pages/LoginPage/Loginpage";
 
